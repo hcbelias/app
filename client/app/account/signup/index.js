6098b9bfd7cd7c0c75af437c3d6285f2240ca393
henrique elias <hcbe2004@gmail.com>
2017-03-12 15:08:10 -0300

    removing old prokject

--- client/app/account/signup/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import angular from 'angular';
-import SignupController from './signup.controller';
-
-export default angular.module('app3App.signup', [])
-  .controller('SignupController', SignupController)
-  .name;
--- client/app/app.constants.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import angular from 'angular';
-
-export default angular.module('app3App.constants', [])
-  .constant('appConfig', require('../../server/config/environment/shared'))
-  .name;
